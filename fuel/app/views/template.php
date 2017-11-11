48f8c176d73de156b5ac637a9a0ff1d035eea1e9
kazua <kazua.kuro@gmail.com>
2017-11-11 20:08:43 +0900

    css, js読み込み訂正

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -9,9 +9,12 @@
   <!-- bootstrap, css -->
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <?= Asset::css(['top.css', 'login.css', 'user.css']); ?>
+
+  <link rel="stylesheet" href="public/assets/top.css" >
+  <link rel="stylesheet" href="public/assets/login.css" >
+  <link rel="stylesheet" href="public/assets/user.css" >
   <!-- JS, JQuery -->
-  <?= Asset::js('jquery.raty.js', 'menu.js'); ?>
+  <script type="text/javascript" href="menu.js"></script>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script></body>
 
