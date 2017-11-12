f1ac9fe0bfc62e8eda02d3b4f5f90f9a45226497
kazua <kazua.kuro@gmail.com>
2017-11-12 11:52:29 +0900

    ドキュメントルート変更実験2

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -10,11 +10,11 @@
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <link rel="stylesheet" href="<?= URI::base().'public/assets/css/top.css'; ?>" >
-  <link rel="stylesheet" href="<?= URI::base().'public/assets/css/login.css'; ?>" >
-  <link rel="stylesheet" href="<?= URI::base().'public/assets/css/user.css'; ?>" >
+  <link rel="stylesheet" href="<?= URI::base().'assets/css/top.css'; ?>" >
+  <link rel="stylesheet" href="<?= URI::base().'assets/css/login.css'; ?>" >
+  <link rel="stylesheet" href="<?= URI::base().'assets/css/user.css'; ?>" >
   <!-- JS, JQuery -->
-  <script type="text/javascript" href="<?= URI::base().'public/assets/js/menu.js'; ?>"></script>
+  <script type="text/javascript" href="<?= URI::base().'assets/js/menu.js'; ?>"></script>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script></body>
 
